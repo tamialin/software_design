c602fee2e8c1a84d53e53903cee35fcb5e2c66d3
heidarsky <heidar078@gmail.com>
2024-03-29 23:43:44 -0500

    add temp db to profile management

--- utils/profileUpdate.py
+++ utils/profileUpdate.py
@@ -1,15 +1,22 @@
 from flask import request, redirect, url_for, render_template, Flask, session
+from utils.temp_db import users_db
 
-users = {
-    'user1': {'username' : 'user1', 'fullname': 'Heidar here', 'address1': '1314', 'address2': 'shadowbrook', 'city': 'Houston', 'states': 'CA', 'zip': '12345'}
-}
+#users = {
+#    'user1': {'username' : 'user1', 'fullname': 'Heidar here', 'address1': '1314', 'address2': 'shadowbrook', 'city': 'Houston', 'states': 'CA', 'zip': '12345'}
+#}
 
 def profileU():
-    username = 'user1'
-    user_data = users.get(username, {})
+    #username = 'user1'
+    #user_data = users.get(username, {})
+
+    if 'username' not in session:
+        return redirect(url_for('login'))
+
+    username = session.get("username")
+    user_data = users_db.get(username, {})
 
     if request.method == 'POST':
-        users [username]= {
+        users_db [username]= {
         'username' : username,
         'fullname' : request.form['fullname'], 
         'address1' : request.form['address1'],
