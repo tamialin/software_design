977cd8ceb279724b0375cf1b6c0f144967394c8e
Emma Pham <kimlien.kitty12@gmail.com>
2024-03-29 23:18:59 -0500

    fully display quote history table using temp DB and quote info

--- utils/fuelModule.py
+++ utils/fuelModule.py
@@ -1,22 +1,27 @@
 from flask import Flask, jsonify, request, redirect, url_for, render_template, session
 from utils.pricing import FuelPricing
 from utils.history import update_quote_history
+from utils.temp_db import users_db
+
 
 def fuelQuote():
    if request.method == 'POST':
-      gallon = float(request.form["gallonsRequested"]) # get amount of gallon from html file
+      gallon = float(request.form["gallonsRequested"])
+      delivery_date = request.form["deliveryDate"] 
       pricingModule = FuelPricing()
       suggested_price, total_price = pricingModule.calculatingPrice(gallon)
 
-      # Assuming you have access to the username through session or some other means
-      username = session.get("username")  # Replace with actual username retrieval logic
+      username = session.get("username") 
 
       # Prepare quote data
       if username:
          quote_data = {
-               'gallon': gallon,
-               'suggested_price': suggested_price,
-               'total_price': total_price
+            'delivery_date': delivery_date,
+            'gallon_requested': gallon,
+            'delivery_address': users_db[username]['address'],
+            'suggested_price': suggested_price,
+            'total_price': total_price,
+               
          }
         # Update quote history
       update_quote_history(username, quote_data)
