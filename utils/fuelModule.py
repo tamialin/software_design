43781f521f4ddcedf2544e705c0c24e0be9a5edc
htnhi <hoangnhi1828@gmail.com>
2024-04-12 03:00:34 -0500

    update quote

--- utils/fuelModule.py
+++ utils/fuelModule.py
@@ -4,17 +4,31 @@ from utils.history import update_quote_history
 from utils.temp_db import users_db
 
 
-def fuelQuote():
+def fuelQuote(mysql):
    if request.method == 'POST':
-      username = session.get("username") 
-      
-      gallon = float(request.form["gallonsRequested"])
-      delivery_date = request.form["deliveryDate"]
-      pricingModule = FuelPricing()
-      suggested_price, total_price = pricingModule.calculatingPrice(gallon)
 
-      # If user hit the Get Quote Button
-      if request.form.get('action') == 'get_quote':
+      # Get username from DB
+      username = session.get("username")
+      cur = mysql.connection.cursor()
+      # cur.execute("SELECT * FROM users WHERE username = %s", (username,))
+      # user = cur.fetchone()
+
+      # Get address from profile
+      cur.execute("SELECT address1, city, states from users WHERE username = %s", (username,))
+      fetchValues = cur.fetchone()
+      if fetchValues:
+         address = fetchValues[0]
+         city = fetchValues[1]
+         state = fetchValues[2]
+         deliveryAddress  = f"{address} - {city} - {state}"
+
+      # Receive input from quote  page
+      gallon = float(request.form["gallonsRequested"])
+      date = request.form["deliveryDate"]
+      pricingModule = FuelPricing()
+      suggested_price, total_price = pricingModule.calculatingPrice(gallon, state)
+
+      # address = request.form["deliveryAddress"]
 
          # # Prepare quote data
          # if username:
@@ -29,16 +43,21 @@ def fuelQuote():
          # update_quote_history(username, quote_data)
 
          # Return the value to display quote
+      if 'displayData' in request.form:
          return jsonify(suggested_price = suggested_price, total_price = total_price)
-      
-      if 'submitBtn' in request.form:
-         date = request.form["deliveryAddress"]
-
-         cur = mysql.connection.cursor()
-         cur.execute("INSERT INTO quote_history (username, date, gallon, address, price, total) VALUE (%s, %s, %f, %s, %s, %s, %s), (username, date, gallon, date, suggested_price, total_price)")
-         
-         mysql.connection.commit()
-         cur.close()
+      elif 'sendData' in request.form:
+         sendToDB(mysql, username, date, gallon, address, suggested_price, total_price)
+         return jsonify(success = True)
 
    # send user input and prices to history backend
    return render_template('quote.html')  
+
+def sendToDB(mysql, username, date, gallon, address, suggested_price, total_price):
+   username = session.get("username")
+   cur = mysql.connection.cursor()
+   cur.execute("SELECT * FROM users WHERE username = %s", (username,))
+   user = cur.fetchone()
+
+   cur.execute("INSERT INTO quote_history (username, date, gallon, address, price, total) VALUES (%s, %s, %s, %s, %s, %s)", (username, date, gallon, address, suggested_price, total_price))
+   mysql.connection.commit()
+   cur.close()
--- utils/pricing.py
+++ utils/pricing.py
@@ -2,17 +2,22 @@ class FuelPricing:
     def __init__(self):
         self.current_price = 1.5
         self.company_profit = 0.1
-        self.location_factor = 0.04
+        self.location_factor = None
         self.history_factor = 0.01
         self.__history = None
 
-    def calculatingPrice(self, gallon, return_customer=False):
+    def calculatingPrice(self, gallon, state, return_customer=False):
         gallon_factor = 0.03 if gallon <= 1000 else 0.02
 
         if return_customer == False:  # Changed to compare with boolean False
             self.__history = 0.0
         else:
             self.__history = self.history_factor
+
+        if state == "TX":
+            self.location_factor = 0.02
+        else:
+            self.location_factor = 0.04
 
         # Calculating the price
         margin = self.location_factor - self.__history + gallon_factor + self.company_profit
