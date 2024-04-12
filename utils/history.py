9958e080b393c7cfb16f36025512f49774e6ac9d
Emma Pham <kimlien.kitty12@gmail.com>
2024-04-11 23:51:00 -0500

    modify get_quote_history function

--- utils/history.py
+++ utils/history.py
@@ -10,7 +10,18 @@ def update_quote_history(username, quote_data):
 
 # Function to get quote history for a user
 def get_quote_history(username):
-    if username in quote_history_db:
-        return quote_history_db[username]
-    else:
-        return []
+    quote_history = []
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM quote_history WHERE username = %s", (username,))
+        quote_history = cursor.fetchall()
+        cursor.close()
+    except Exception as e:
+        print("Error fething quote history: ", str(e))
+    return quote_history
+
+
+    # if username in quote_history_db:
+    #     return quote_history_db[username]
+    # else:
+    #     return []
