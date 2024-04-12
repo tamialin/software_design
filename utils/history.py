c358799283dacce0b38732315ff79de44e5d8eef
Emma Pham <kimlien.kitty12@gmail.com>
2024-04-12 12:52:48 -0500

    retrieve data from DB to display on history page for each user

--- utils/history.py
+++ utils/history.py
@@ -1,15 +1,7 @@
 from utils.temp_db import quote_history_db
 
-# Function to update quote history for a user
-def update_quote_history(username, quote_data):
-    if username in quote_history_db:
-        quote_history_db[username].append(quote_data)
-        return True
-    else:
-        return False
-
 # Function to get quote history for a user
-def get_quote_history(username):
+def get_quote_history(username, mysql):
     quote_history = []
     try:
         cursor = mysql.connection.cursor()
@@ -20,8 +12,3 @@ def get_quote_history(username):
         print("Error fething quote history: ", str(e))
     return quote_history
 
-
-    # if username in quote_history_db:
-    #     return quote_history_db[username]
-    # else:
-    #     return []
