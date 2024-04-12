6864c20cdb7486e2f6900c89b07b9e19f8fc0893
htnhi <hoangnhi1828@gmail.com>
2024-04-12 16:24:24 -0500

    fix address part on Quote

--- utils/fuelModule.py
+++ utils/fuelModule.py
@@ -5,22 +5,29 @@ from utils.temp_db import users_db
 
 
 def fuelQuote(mysql):
-   if request.method == 'POST':
+   username = session.get("username")
+   cur = mysql.connection.cursor()
+   # dAddress = "This address"
+   cur.execute("SELECT address1, city, states, zip from users WHERE username = %s", (username,))
+   fetchValues = cur.fetchone()
+   
+   if fetchValues:
+      address = fetchValues[0]
+      city = fetchValues[1]
+      state = fetchValues[2]
+      zipCode = fetchValues[3]
+      dAddress  = f"{address} - {city} - {state} - {zipCode}"
+   else:
+      dAddress = "Address Hasn't Been Set Up. Please Update Your Profile"
 
+   if request.method == 'POST':
       # Get username from DB
-      username = session.get("username")
-      cur = mysql.connection.cursor()
+      
       # cur.execute("SELECT * FROM users WHERE username = %s", (username,))
       # user = cur.fetchone()
+      # session['dAddress'] = dAddress
 
       # Get address from profile
-      cur.execute("SELECT address1, city, states from users WHERE username = %s", (username,))
-      fetchValues = cur.fetchone()
-      if fetchValues:
-         address = fetchValues[0]
-         city = fetchValues[1]
-         state = fetchValues[2]
-         deliveryAddress  = f"{address} - {city} - {state}"
 
       # Receive input from quote  page
       gallon = float(request.form["gallonsRequested"])
@@ -42,15 +49,14 @@ def fuelQuote(mysql):
          # # Update quote history
          # update_quote_history(username, quote_data)
 
-         # Return the value to display quote
+      # Return the value to display quote
       if 'displayData' in request.form:
          return jsonify(suggested_price = suggested_price, total_price = total_price)
-      elif 'sendData' in request.form:
+      elif 'sendData' in request.form: # send user input and prices to history backend
          sendToDB(mysql, username, date, gallon, address, suggested_price, total_price)
          return jsonify(success = True)
-
-   # send user input and prices to history backend
-   return render_template('quote.html')  
+      
+   return render_template('quote.html', dAddress = dAddress)  
 
 def sendToDB(mysql, username, date, gallon, address, suggested_price, total_price):
    username = session.get("username")
