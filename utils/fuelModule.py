df8460414b65c64d29d239ce72536c5da327f83c
htnhi <hoangnhi1828@gmail.com>
2024-04-18 16:13:37 -0500

    CleanUp code /quote, update history

--- utils/fuelModule.py
+++ utils/fuelModule.py
@@ -7,9 +7,9 @@ def fuelQuote(mysql):
    cur = mysql.connection.cursor()
 
    #Get address from DB
-   cur.execute("SELECT address1, city, states, zip from users WHERE username = %s", (username,))
+   cur.execute("SELECT address1, city, states, zip, history from users WHERE username = %s", (username,))
    fetchValues = cur.fetchone()
-   if fetchValues:
+   if fetchValues[0] != None:
       address = fetchValues[0]
       city = fetchValues[1]
       state = fetchValues[2]
@@ -17,37 +17,27 @@ def fuelQuote(mysql):
       dAddress  = f"{address}, {city}, {state}, {zipCode}"
    else: 
       dAddress = "Address Hasn't Been Set Up. Please Update Your Profile"
+   returnUser = fetchValues[4]
+   
+   # Receive input from quote page
 
    if request.method == 'POST':
-      # Get username from DB
-      username = session.get("username")
-
-   # Get address from profile
-   cur.execute("SELECT address1, city, states, zip from users WHERE username = %s", (username,))
-   fetchValues = cur.fetchone()
-   if fetchValues:
-      address = fetchValues[0]
-      city = fetchValues[1]
-      state = fetchValues[2]
-      zipCode = fetchValues[3]
-      dAddress  = f"{address} - {city} - {state} - {zipCode}"
-   else:
-      dAddress = "Address Hasn't Not Been Set Up. Please Update Your Profile"
-
-   if request.method == 'POST':
-      # Receive input from quote  page
       gallon = float(request.form["gallonsRequested"])
       date = request.form["deliveryDate"]
       pricingModule = FuelPricing()
-      suggested_price, total_price = pricingModule.calculatingPrice(gallon, state)
+      suggested_price, total_price = pricingModule.calculatingPrice(gallon, state, returnUser)
 
       # Return the value to display quote
       if 'displayData' in request.form:
          return jsonify(suggested_price = suggested_price, total_price = total_price)
       elif 'sendData' in request.form: # send user input and prices to history backend
          sendToDB(mysql, username, date, gallon, address, suggested_price, total_price)
+         if returnUser == 0:
+            cur.execute("UPDATE users SET history = %s WHERE username = %s", (1, username)) #Update user history factor
+            mysql.connection.commit()
+         cur.close()
          return jsonify(success = True)
-      
+   
    return render_template('quote.html', dAddress = dAddress)  
 
 def sendToDB(mysql, username, date, gallon, address, suggested_price, total_price):
@@ -58,4 +48,3 @@ def sendToDB(mysql, username, date, gallon, address, suggested_price, total_pric
 
    cur.execute("INSERT INTO quote_history (username, date, gallon, address, price, total) VALUES (%s, %s, %s, %s, %s, %s)", (username, date, gallon, address, suggested_price, total_price))
    mysql.connection.commit()
-   cur.close()
--- utils/pricing.py
+++ utils/pricing.py
@@ -1,18 +1,19 @@
+
+
 class FuelPricing:
     def __init__(self):
         self.current_price = 1.5
         self.company_profit = 0.1
         self.location_factor = None
-        self.history_factor = 0.01
-        self.__history = None
+        self.history_factor = None
 
-    def calculatingPrice(self, gallon, state, return_customer=False):
+    def calculatingPrice(self, gallon, state, returnUser):
         gallon_factor = 0.03 if gallon <= 1000 else 0.02
 
-        if return_customer == False:  # Changed to compare with boolean False
-            self.__history = 0.0
+        if returnUser == 0:  # Changed to compare with boolean False
+            self.history_factor = 0.0
         else:
-            self.__history = self.history_factor
+            self.history_factor = 0.01
 
         if state == "TX":
             self.location_factor = 0.02
@@ -20,8 +21,10 @@ class FuelPricing:
             self.location_factor = 0.04
 
         # Calculating the price
-        margin = self.location_factor - self.__history + gallon_factor + self.company_profit
+        margin = self.location_factor - self.history_factor + gallon_factor + self.company_profit
         suggested_price = round(margin * self.current_price, 2)
         total_price = round(float(gallon * suggested_price), 2)
+
+        print(self.history_factor)
 
         return suggested_price, total_price
