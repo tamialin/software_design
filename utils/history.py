2fa2edb41f58f81f6b79eb8821a4daabb55e1dca
Emma Pham <kimlien.kitty12@gmail.com>
2024-04-19 13:32:46 -0500

    update history table and data

--- utils/history.py
+++ utils/history.py
@@ -5,7 +5,8 @@ def get_quote_history(username, mysql):
     quote_history = []
     try:
         cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM quote_history WHERE username = %s", (username,))
+        cursor.execute("SELECT quote_history.*, users.city, users.zip, users.states FROM quote_history JOIN users ON quote_history.username = users.username WHERE quote_history.username = %s", (username,))
+        #cursor.execute("SELECT * FROM quote_history WHERE username = %s", (username,))
         quote_history = cursor.fetchall()
         cursor.close()
     except Exception as e:
